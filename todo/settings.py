67cb0366ab49f8f6d338dd9d94b1dbe5895236d8
Kedarious <darious_petway@yahoo.com>
2020-11-27 13:51:05 -0600

    Update

--- todo/settings.py
+++ todo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\KeDarious Petway\Desktop\django\todo\tasks\template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
